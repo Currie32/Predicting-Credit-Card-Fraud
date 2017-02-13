5b62ff6e1eb48dc43e7809f6db5a5f3f0491e9e3
David Currie <david.currie32@gmail.com>
2017-02-12 20:39:06 -0500

    Add files via upload

---
+++ Predicting_Credit_Card_Fraud.py
@@ -0,0 +1,545 @@
+
+# coding: utf-8
+
+# # Predicting Credit Card Fraud
+
+# The goal for this analysis is to predict credit card fraud in the transactional data. I will be using tensorflow to build the predictive model, and t-SNE to visualize the dataset in two dimensions at the end of this analysis. If you would like to learn more about the data, visit: https://www.kaggle.com/dalpozz/creditcardfraud. 
+# 
+# The sections of this analysis include: Exploring the Data, Building the Neural Network, Visualizing the Data with t-SNE, and Summary.
+
+# In[4]:
+
+import pandas as pd
+import numpy as np 
+import tensorflow as tf
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.gridspec as gridspec
+from sklearn.preprocessing import StandardScaler
+from sklearn.manifold import TSNE
+
+
+# In[17]:
+
+df = pd.read_csv("creditcard.csv")
+
+
+# ## Exploring the Data
+
+# In[18]:
+
+df.head()
+
+
+# The data is mostly transformed from its original form, for confidentiality reasons.
+
+# In[19]:
+
+df.describe()
+
+
+# In[20]:
+
+df.isnull().sum()
+
+
+# No missing values, that makes things a little easier.
+# 
+# Let's see how time compares across fradulent and normal transactions.
+
+# In[21]:
+
+print ("Fraud")
+print (df.Time[df.Class == 1].describe())
+print ()
+print ("Normal")
+print (df.Time[df.Class == 0].describe())
+
+
+# In[22]:
+
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10,4))
+
+bins = 50
+
+ax1.hist(df.Time[df.Class == 1], bins = bins)
+ax1.set_title('Fraud')
+
+ax2.hist(df.Time[df.Class == 0], bins = bins)
+ax2.set_title('Normal')
+
+plt.xlabel('Time (in Seconds)')
+plt.ylabel('Number of Transactions')
+plt.show()
+
+
+# The 'Time' feature looks pretty similar across both types of transactions. You could argue that fraudulent transactions are more uniformly distributed, while normal transactions have a cyclical distribution. This could make it easier to detect a fraudulent transaction during at an 'off-peak' time.
+
+# Now let's see if the transaction amount differs between the two types.
+
+# In[23]:
+
+print ("Fraud")
+print (df.Amount[df.Class == 1].describe())
+print ()
+print ("Normal")
+print (df.Amount[df.Class == 0].describe())
+
+
+# In[24]:
+
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10,4))
+
+bins = 30
+
+ax1.hist(df.Amount[df.Class == 1], bins = bins)
+ax1.set_title('Fraud')
+
+ax2.hist(df.Amount[df.Class == 0], bins = bins)
+ax2.set_title('Normal')
+
+plt.xlabel('Amount ($)')
+plt.ylabel('Number of Transactions')
+plt.yscale('log')
+plt.show()
+
+
+# In[25]:
+
+df['Amount_max_fraud'] = 1
+df.loc[df.Amount <= 2125.87, 'Amount_max_fraud'] = 0
+
+
+# Most transactions are small amounts, less than \$100. Fraudulent transactions have a maximum value far less than normal transactions, \$2,125.87 vs \$25,691.16.
+# 
+# Let's compare Time with Amount and see if we can learn anything new.
+
+# In[26]:
+
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10,6))
+
+ax1.scatter(df.Time[df.Class == 1], df.Amount[df.Class == 1])
+ax1.set_title('Fraud')
+
+ax2.scatter(df.Time[df.Class == 0], df.Amount[df.Class == 0])
+ax2.set_title('Normal')
+
+plt.xlabel('Time (in Seconds)')
+plt.ylabel('Amount')
+plt.show()
+
+
+# Nothing too useful here. 
+# 
+# Next, let's take a look at the anonymized features.
+
+# In[27]:
+
+#Select only the anonymized features.
+v_features = df.ix[:,1:29].columns
+
+
+# In[28]:
+
+plt.figure(figsize=(12,28*4))
+gs = gridspec.GridSpec(28, 1)
+for i, cn in enumerate(df[v_features]):
+    ax = plt.subplot(gs[i])
+    sns.distplot(df[cn][df.Class == 1], bins=50)
+    sns.distplot(df[cn][df.Class == 0], bins=50)
+    ax.set_xlabel('')
+    ax.set_title('histogram of feature: ' + str(cn))
+plt.show()
+
+
+# In[29]:
+
+#Drop all of the features that have very similar distributions between the two types of transactions.
+df = df.drop(['V28','V27','V26','V25','V24','V23','V22','V20','V15','V13','V8'], axis =1)
+
+
+# In[30]:
+
+#Based on the plots above, these features are created to identify values where fraudulent transaction are more common.
+df['V1_'] = df.V1.map(lambda x: 1 if x < -3 else 0)
+df['V2_'] = df.V2.map(lambda x: 1 if x > 2.5 else 0)
+df['V3_'] = df.V3.map(lambda x: 1 if x < -4 else 0)
+df['V4_'] = df.V4.map(lambda x: 1 if x > 2.5 else 0)
+df['V5_'] = df.V5.map(lambda x: 1 if x < -4.5 else 0)
+df['V6_'] = df.V6.map(lambda x: 1 if x < -2.5 else 0)
+df['V7_'] = df.V7.map(lambda x: 1 if x < -3 else 0)
+df['V9_'] = df.V9.map(lambda x: 1 if x < -2 else 0)
+df['V10_'] = df.V10.map(lambda x: 1 if x < -2.5 else 0)
+df['V11_'] = df.V11.map(lambda x: 1 if x > 2 else 0)
+df['V12_'] = df.V12.map(lambda x: 1 if x < -2 else 0)
+df['V14_'] = df.V14.map(lambda x: 1 if x < -2.5 else 0)
+df['V16_'] = df.V16.map(lambda x: 1 if x < -2 else 0)
+df['V17_'] = df.V17.map(lambda x: 1 if x < -2 else 0)
+df['V18_'] = df.V18.map(lambda x: 1 if x < -2 else 0)
+df['V19_'] = df.V19.map(lambda x: 1 if x > 1.5 else 0)
+df['V21_'] = df.V21.map(lambda x: 1 if x > 0.6 else 0)
+
+
+# In[31]:
+
+#Create a new feature for normal (non-fraudulent) transactions.
+df.loc[df.Class == 0, 'Normal'] = 1
+df.loc[df.Class == 1, 'Normal'] = 0
+
+
+# In[32]:
+
+#Rename 'Class' to 'Fraud'.
+df = df.rename(columns={'Class': 'Fraud'})
+
+
+# In[33]:
+
+#492 fraudulent transactions, 284,315 normal transactions.
+#0.172% of transactions were fraud. 
+print(df.Normal.value_counts())
+print()
+print(df.Fraud.value_counts())
+
+
+# In[64]:
+
+pd.set_option("display.max_columns",101)
+df.head()
+
+
+# In[35]:
+
+#Create dataframes of only Fraud and Normal transactions.
+Fraud = df[df.Fraud == 1]
+Normal = df[df.Normal == 1]
+
+
+# In[36]:
+
+#Set X_train equal to 75% of the fraudulent transactions.
+X_train = Fraud.sample(frac=0.75)
+count_Frauds = len(X_train)
+
+#Add 75% of the normal transactions to X_train.
+X_train = pd.concat([X_train, Normal.sample(frac = 0.75)], axis = 0)
+
+#X_test contains all the transaction not in X_train.
+X_test = df.loc[~df.index.isin(X_train.index)]
+
+
+# In[37]:
+
+#Shuffle the dataframes so that the training is done in a random order.
+X_train = shuffle(X_train)
+X_test = shuffle(X_test)
+
+
+# In[38]:
+
+#Add our target features to y_train and y_test.
+y_train = X_train.Fraud
+y_train = pd.concat([y_train, X_train.Normal], axis=1)
+
+y_test = X_test.Fraud
+y_test = pd.concat([y_test, X_test.Normal], axis=1)
+
+
+# In[39]:
+
+#Drop target features from X_train and X_test.
+X_train = X_train.drop(['Fraud','Normal'], axis = 1)
+X_test = X_test.drop(['Fraud','Normal'], axis = 1)
+
+
+# In[40]:
+
+#Check to ensure all of the training/testing dataframes are of the correct length
+print(len(X_train))
+print(len(y_train))
+print(len(X_test))
+print(len(y_test))
+
+
+# In[41]:
+
+'''
+Due to the imbalance in the data, ratio will act as an equal weighting system for our model. 
+By dividing the number of transactions by those that are fraudulent, ratio will equal the value that when multiplied
+by the number of fraudulent transactions will equal the number of normal transaction. 
+Simply put: # of fraud * ratio = # of normal
+'''
+ratio = len(X_train)/count_Frauds 
+
+y_train.Fraud *= ratio
+y_test.Fraud *= ratio
+
+
+# In[42]:
+
+#Names of all of the features in X_train.
+features = X_train.columns.values
+
+#Transform each feature in features so that it has a mean of 0 and standard deviation of 1; 
+#this helps with training the neural network.
+for feature in features:
+    mean, std = df[feature].mean(), df[feature].std()
+    X_train.loc[:, feature] = (X_train[feature] - mean) / std
+    X_test.loc[:, feature] = (X_test[feature] - mean) / std
+
+
+# ## Train the Neural Net
+
+# In[43]:
+
+inputX = X_train.as_matrix()
+inputY = y_train.as_matrix()
+inputX_test = X_test.as_matrix()
+inputY_test = y_test.as_matrix()
+
+
+# In[149]:
+
+#Number of input nodes.
+input_nodes = 37
+
+#Multiplier maintains a fixed ratio of nodes between each layer.
+mulitplier = 1.5 
+
+#Number of nodes in each hidden layer
+hidden_nodes1 = 15
+hidden_nodes2 = round(hidden_nodes1 * mulitplier)
+hidden_nodes3 = round(hidden_nodes2 * mulitplier)
+
+#Percent of nodes to keep during dropout.
+pkeep = 0.9
+
+
+# In[150]:
+
+#input
+x = tf.placeholder(tf.float32, [None, input_nodes])
+
+#layer 1
+W1 = tf.Variable(tf.truncated_normal([input_nodes, hidden_nodes1], stddev = 0.1))
+b1 = tf.Variable(tf.zeros([hidden_nodes1]))
+y1 = tf.nn.sigmoid(tf.matmul(x, W1) + b1)
+
+#layer 2
+W2 = tf.Variable(tf.truncated_normal([hidden_nodes1, hidden_nodes2], stddev = 0.1))
+b2 = tf.Variable(tf.zeros([hidden_nodes2]))
+y2 = tf.nn.sigmoid(tf.matmul(y1, W2) + b2)
+
+#layer 3
+W3 = tf.Variable(tf.truncated_normal([hidden_nodes2, hidden_nodes3], stddev = 0.1)) 
+b3 = tf.Variable(tf.zeros([hidden_nodes3]))
+y3 = tf.nn.sigmoid(tf.matmul(y2, W3) + b3)
+y3 = tf.nn.dropout(y3, pkeep)
+
+#layer 4
+W4 = tf.Variable(tf.truncated_normal([hidden_nodes3, 2], stddev = 0.1)) 
+b4 = tf.Variable(tf.zeros([2]))
+y4 = tf.nn.softmax(tf.matmul(y3, W4) + b4)
+
+#output
+y = y4
+y_ = tf.placeholder(tf.float32, [None, 2])
+
+
+# In[157]:
+
+#Parameters
+training_epochs = 2000
+display_step = 50
+n_samples = y_train.size
+
+batch = tf.Variable(0)
+
+learning_rate = tf.train.exponential_decay(
+  0.01,              #Base learning rate.
+  batch,             #Current index into the dataset.
+  len(inputX),       #Decay step.
+  0.95,              #Decay rate.
+  staircase=False)
+
+
+# In[158]:
+
+#Cost function: Cross Entropy
+cost = -tf.reduce_sum(y_ * tf.log(y))
+
+#We will optimize our model via AdamOptimizer
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+
+#Correct prediction if the most likely value (Fraud or Normal) from softmax equals the target value.
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+# In[159]:
+
+#Initialize variables and tensorflow session
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+
+# In[160]:
+
+accuracy_summary = [] #Record accuracy values for plot
+cost_summary = [] #Record cost values for plot
+
+for i in range(training_epochs):  
+    sess.run([optimizer], feed_dict={x: inputX, y_: inputY})
+    
+    # Display logs per epoch step
+    if (i) % display_step == 0:
+        train_accuracy, newCost = sess.run([accuracy, cost], feed_dict={x: inputX, y_: inputY})
+        print ("Training step:", i,
+               "Accuracy =", "{:.5f}".format(train_accuracy), 
+               "Cost = ", "{:.5f}".format(newCost))
+        accuracy_summary.append(train_accuracy)
+        cost_summary.append(newCost)
+        
+print()
+print ("Optimization Finished!")
+training_accuracy = sess.run(accuracy, feed_dict={x: inputX, y_: inputY})
+print ("Training Accuracy=", training_accuracy)
+print()
+testing_accuracy = sess.run(accuracy, feed_dict={x: inputX_test, y_: inputY_test})
+print ("Testing Accuracy=", testing_accuracy)
+
+
+# In[161]:
+
+#Plot accuracy and cost summary
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10,4))
+
+ax1.plot(accuracy_summary)
+ax1.set_title('Accuracy')
+
+ax2.plot(cost_summary)
+ax2.set_title('Cost')
+
+plt.xlabel('Epochs (x50)')
+plt.show()
+
+
+# In[163]:
+
+#Find the predicted values, then use them to build a confusion matrix
+predicted = tf.argmax(y, 1)
+testing_predictions = sess.run(predicted, feed_dict={x: inputX_test, y_:inputY_test})
+
+confusion_matrix(inputY_test[:,1], testing_predictions)
+
+
+# To summarize the confusion matrix: 
+# 
+# Correct Fraud: 102
+# 
+# Incorrect Fraud: 21
+# 
+# Correct Normal: 71,005
+# 
+# Incorrect Normal: 74
+
+# Although the neural network can detect most of the fraudulent transactions (82.93%), there are still some that got away. About 0.10% of normal transactions were classified as fraudulent, which can unfortunately add up very quickly given the large number of credit card transactions that occur each minute/hour/day. Nonetheless, this models performs reasonably well and I expect that if we had more data, and if the features were not pre-transformed, we could have created new features, and built a more useful neural network.  
+
+# ## Visualizing the Data with t-SNE
+
+# First we are going to use t-SNE with the original data, then with the data we used for training our neural network. I expect/hope that the second scatter plot will show a clearer contrast between the normal and the fraudulent transactions. If this is the case, its signals that the work done during the feature engineering stage of the analysis was beneficial to helping the neural network understand the data.
+
+# In[5]:
+
+#reload the original dataset
+tsne_data = pd.read_csv("creditcard.csv")
+
+
+# In[6]:
+
+#Set df2 equal to all of the fraulent and 10,000 normal transactions.
+df2 = tsne_data[tsne_data.Class == 1]
+df2 = pd.concat([df2, tsne_data[tsne_data.Class == 0].sample(n = 10000)], axis = 0)
+
+
+# In[7]:
+
+#Scale features to improve the training ability of TSNE.
+standard_scaler = StandardScaler()
+df2_std = standard_scaler.fit_transform(df2)
+
+#Set y equal to the target values.
+y = df2.ix[:,-1].values
+
+
+# In[52]:
+
+tsne = TSNE(n_components=2, random_state=0)
+x_test_2d = tsne.fit_transform(df2_std)
+
+
+# In[56]:
+
+#Build the scatter plot with the two types of transactions.
+color_map = {0:'red', 1:'blue'}
+plt.figure()
+for idx, cl in enumerate(np.unique(y)):
+    plt.scatter(x = x_test_2d[y==cl,0], 
+                y = x_test_2d[y==cl,1], 
+                c = color_map[idx], 
+                label = cl)
+plt.xlabel('X in t-SNE')
+plt.ylabel('Y in t-SNE')
+plt.legend(loc='upper left')
+plt.title('t-SNE visualization of test data')
+plt.show()
+
+
+# The are two main groupings of fraudulent transactions, while the remaineder are mixed within the rest of the data.
+# 
+# Note: I have only used 10,000 of the 284,315 normal transactions for this visualization. I would have liked to of used more, but my laptop crashes if many more than 10,000 transactions are included. With only 3.15% of the data being used, there should be some accuracy to this plot, but I am confident that the layout would look different if all of the transactions were included.
+
+# In[59]:
+
+#Set df_used to the fraudulent transactions' dataset.
+df_used = Fraud
+
+#Add 10,000 normal transactions to df_used.
+df_used = pd.concat([df_used, Normal.sample(n = 10000)], axis = 0)
+
+
+# In[60]:
+
+#Scale features to improve the training ability of TSNE.
+df_used_std = standard_scaler.fit_transform(df_used)
+
+#Set y_used equal to the target values.
+y_used = df_used.ix[:,-1].values
+
+
+# In[61]:
+
+x_test_2d_used = tsne.fit_transform(df_used_std)
+
+
+# In[62]:
+
+color_map = {1:'red', 0:'blue'}
+plt.figure()
+for idx, cl in enumerate(np.unique(y_used)):
+    plt.scatter(x=x_test_2d_used[y_used==cl,0], 
+                y=x_test_2d_used[y_used==cl,1], 
+                c=color_map[idx], 
+                label=cl)
+plt.xlabel('X in t-SNE')
+plt.ylabel('Y in t-SNE')
+plt.legend(loc='upper left')
+plt.title('t-SNE visualization of test data')
+plt.show()
+
+
+# It appears that the work we did in the feature engineering stage of this analysis has been for the best. We can see that the fraudulent transactions are all part of a group of points. This suggests that it is easier for a model to identify the fraudulent transactions in the testing data, and to learn about the traits of the fraudulent transactions in the training data. 
